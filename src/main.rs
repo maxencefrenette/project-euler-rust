aa2c504f55810a7b9926b99d2014dc14e5997824
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-25 02:35:37 -0400

    Make code more DRY

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-use solutions::*;
+use solutions::SOLVERS;
 use std::env;
 
 mod common;
 mod solutions;
 
 fn main() {
-    const USAGE: &str = "To run a solution, execute `cargo run -- x`, where x is the problem id.";
-    let arg = env::args().nth(1).expect(USAGE);
+    let arg = env::args()
+        .nth(1)
+        .expect("Usage: `cargo run -- x`, where x is the problem id.");
 
-    let solution = match arg.as_str() {
-        "1" => solve_1(),
-        "2" => solve_2(),
-        "3" => solve_3(),
-        "4" => solve_4(),
-        "5" => solve_5(),
-        "6" => solve_6(),
-        "25" => solve_25(),
-        _ => panic!(USAGE),
-    };
-
-    println!("{}", solution);
+    if let Some(solver) = SOLVERS.get(arg.as_str()) {
+        println!("{}", solver());
+    } else {
+        println!("Problem {} isn't solved yet.", arg);
+    }
 }
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
 mod sol_1;
 mod sol_2;
 mod sol_25;
@@ -5,11 +8,18 @@ mod sol_3;
 mod sol_4;
 mod sol_5;
 mod sol_6;
+lazy_static! {
+    pub static ref SOLVERS: HashMap<&'static str, fn() -> u64> = {
+        let mut m = HashMap::new();
 
-pub use sol_1::solve as solve_1;
-pub use sol_2::solve as solve_2;
-pub use sol_25::solve as solve_25;
-pub use sol_3::solve as solve_3;
-pub use sol_4::solve as solve_4;
-pub use sol_5::solve as solve_5;
-pub use sol_6::solve as solve_6;
+        m.insert("1", sol_1::solve as fn() -> u64);
+        m.insert("2", sol_2::solve as fn() -> u64);
+        m.insert("3", sol_3::solve as fn() -> u64);
+        m.insert("4", sol_4::solve as fn() -> u64);
+        m.insert("5", sol_5::solve as fn() -> u64);
+        m.insert("6", sol_6::solve as fn() -> u64);
+        m.insert("25", sol_25::solve as fn() -> u64);
+
+        m
+    };
+}
