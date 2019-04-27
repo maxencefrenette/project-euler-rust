b9fdb05e448301c3d48a7811b99d06cb6f3e27a5
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-27 19:38:32 -0400

    Move totient function to common

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,5 @@
+use primal::Sieve;
+
 pub struct Fibonacci {
     a: u64,
     b: u64,
@@ -17,4 +19,29 @@ impl Iterator for Fibonacci {
 
 pub fn fibonacci_iter() -> Fibonacci {
     Fibonacci { a: 0, b: 1 }
+}
+
+pub fn totient(n: u64, sieve: &Sieve) -> u64 {
+    let factors = sieve.factor(n as usize).unwrap();
+
+    factors
+        .into_iter()
+        .map(|(p_usize, k_usize)| {
+            let p = p_usize as u64;
+            let k = k_usize as u32;
+
+            p.pow(k) - p.pow(k - 1)
+        })
+        .product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn totient_test() {
+        let sieve = Sieve::new(30);
+        assert_eq!(totient(780, &sieve), 192);
+    }
 }
--- src/solutions/sol_69.rs
+++ src/solutions/sol_69.rs
@@ -1,19 +1,6 @@
+use crate::common::totient;
 use float_ord::FloatOrd;
 use primal::Sieve;
-
-fn phi(n: u64, sieve: &Sieve) -> u64 {
-    let factors = sieve.factor(n as usize).unwrap();
-
-    factors
-        .into_iter()
-        .map(|(p_usize, k_usize)| {
-            let p = p_usize as u64;
-            let k = k_usize as u32;
-
-            p.pow(k) - p.pow(k - 1)
-        })
-        .product()
-}
 
 pub fn solve() -> u64 {
     let sieve = Sieve::new(1000);
@@ -21,6 +8,6 @@ pub fn solve() -> u64 {
     println!("{}", sieve.upper_bound());
 
     (2..=1_000_000)
-        .max_by_key(|n| FloatOrd((*n as f64) / (phi(*n, &sieve) as f64)))
+        .max_by_key(|n| FloatOrd((*n as f64) / (totient(*n, &sieve) as f64)))
         .unwrap()
 }
